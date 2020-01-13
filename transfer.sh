8198ea2c217d09abd6913c480f3702f2eabc3a52
Per Forser <per@pkdata.se>
2020-01-13 07:14:10 +0100

    transfer.sh

--- transfer.sh
+++ transfer.sh
@@ -7,16 +7,24 @@ transfer() {
         return 1
     fi
     tmpfile=$(mktemp)
+    file="$1"
+    basefile=$(basename "$file" | sed -e 's/[^a-zA-Z0-9._-]/-/g')
     if tty -s
     then
-        basefile=$(basename "$1" | sed -e 's/[^a-zA-Z0-9._-]/-/g')
-        curl --progress-bar --upload-file "$1" "https://transfer.sh/$basefile" >> $tmpfile
+        if [ -d "$file" ]
+        then
+            basefile="${basefile}.zip"
+            (cd "$file" && zip -r -q - .) |
+                curl --progress-bar --upload-file "-" "https://transfer.sh/$basefile" >> $tmpfile
+        else
+            curl --progress-bar --upload-file "$file" "https://transfer.sh/$basefile" >> $tmpfile
+        fi
     else
-        curl --progress-bar --upload-file "-" "https://transfer.sh/$1" >> $tmpfile 
+        curl --progress-bar --upload-file "-" "https://transfer.sh/$basefile" >> $tmpfile 
     fi
     cat $tmpfile
-	echo
-	printf "[%s] %s\n" "$(date --rfc-3339=seconds)" "$(cat $tmpfile)" >> "$HOME/.transfers"
+    echo
+    printf "[%s] %s\n" "$(date --rfc-3339=seconds)" "$(cat $tmpfile)" >> "$HOME/.transfers"
     rm -f $tmpfile
 }
 
